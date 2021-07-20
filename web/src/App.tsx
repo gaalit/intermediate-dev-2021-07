b618541a5569c251b37a58b5ba8e18bb114adbbd
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-07-20 10:43:34 -0400

    added function fetchData that connects to the api, fetches data and sets state with data fetched

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 export function App() {
+  const [posts, setPosts] = useState([]);
+
+  const fetchData = () => {
+    axios
+      .get('http://localhost:4000/posts')
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
