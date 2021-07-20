3b8c79ac6bf771833977ac010ae030297188fc63
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-07-20 14:39:36 -0400

    added button that shows list of all posts when clicked

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,11 +5,19 @@ import publishDateReverseOrder from './helperFunctions/publishDateReverseOrder';
 // import showUniqueAuthor from './helperFunctions/showUniqueAuthor';
 import Posts from './Posts';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 export const App = () => {
   const [posts, setPosts] = useState([]);
 
+  // state to toggle view of all posts
+  const [viewAllPosts, setViewAllPosts] = useState(false);
+
   console.log(posts);
+
+  const viewPosts = () => {
+    setViewAllPosts(!viewAllPosts);
+  };
 
   const fetchData = () => {
     axios
@@ -24,5 +32,10 @@ export const App = () => {
     fetchData();
   }, []);
 
-  return <Posts posts={posts} />;
+  return (
+    <>
+      <Button onClick={viewPosts}>View All Posts</Button>
+      {viewAllPosts && <Posts posts={posts} />}
+    </>
+  );
 };
