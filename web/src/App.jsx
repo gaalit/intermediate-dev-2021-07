a70264c076edca358df60c25b6a9b96ad7119910
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-07-20 16:49:21 -0400

    updated App component to show unique authors when applicable

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import publishDateReverseOrder from './helperFunctions/publishDateReverseOrder';
-// import showUniqueAuthor from './helperFunctions/showUniqueAuthor';
+import showUniqueAuthor from './helperFunctions/showUniqueAuthor';
 import Posts from './Posts';
-import './App.css';
+import Authors from './Authors';
 import { Button } from '@material-ui/core';
 
 export const App = () => {
@@ -13,12 +13,18 @@ export const App = () => {
   // state to toggle view of all posts
   const [viewAllPosts, setViewAllPosts] = useState(false);
 
-  console.log(posts);
+  // state to toggle view by author
+  const [viewAllAuthors, setViewAllAuthors] = useState(false);
 
   const viewPosts = () => {
     setViewAllPosts(!viewAllPosts);
   };
 
+  const viewAuthors = () => {
+    setViewAllAuthors(!viewAllAuthors);
+  };
+
+  // fetches data from api
   const fetchData = () => {
     axios
       .get('http://localhost:4000/posts')
@@ -28,14 +34,23 @@ export const App = () => {
       .catch((err) => console.log(err));
   };
 
+  // uses data from api to filter through unique authors
+  const authors = showUniqueAuthor(posts);
+
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <>
-      <Button onClick={viewPosts}>View All Posts</Button>
+      <Button onClick={viewPosts} color="secondary">
+        View All Posts
+      </Button>
       {viewAllPosts && <Posts posts={posts} />}
+      <Button onClick={viewAuthors} color="secondary">
+        Select by Author
+      </Button>
+      {viewAllAuthors && <Authors authors={authors} posts={posts} />}
     </>
   );
 };
