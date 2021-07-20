99e774f896bb2136300480eaff0c81fe4c417365
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-07-20 14:07:51 -0400

    added PostsList component to App

---
+++ web/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import publishDateReverseOrder from './helperFunctions/publishDateReverseOrder';
+// import showUniqueAuthor from './helperFunctions/showUniqueAuthor';
+import PostsList from './Posts';
+import './App.css';
+
+export const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  console.log(posts);
+
+  const fetchData = () => {
+    axios
+      .get('http://localhost:4000/posts')
+      .then((res) => {
+        setPosts(publishDateReverseOrder(res.data));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <PostsList posts={posts} />;
+};
