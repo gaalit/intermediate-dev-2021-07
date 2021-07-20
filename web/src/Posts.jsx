f9ed111aa5a14f1e8d57a6b13ff973ef0079654b
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-07-20 19:08:56 -0400

    added error boundary to Posts component and made jest test pass

--- web/src/Posts.jsx
+++ web/src/Posts.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import Post from './Post';
 
-const PostsList = (props) => {
+const Posts = (props) => {
   const { posts } = props;
-  console.log('POSTS', posts);
 
   return (
     <>
-      <h3>All Posts:</h3>
-      {posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            body={post.body}
-            author={post.author}
-            date={post.publishedAt}
-          />
-        );
-      })}
+      {posts &&
+        posts.map((post) => {
+          return (
+            <Post
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              author={post.author}
+              date={post.publishedAt}
+            />
+          );
+        })}
     </>
   );
 };
 
-export default PostsList;
+export default Posts;
